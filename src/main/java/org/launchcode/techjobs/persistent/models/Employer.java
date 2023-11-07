948e6325370b561496c13f14e5f0d575cee79e0e
MrsKing2023 <smarshall84@yahoo.com>
2023-11-06 21:32:04 -0500

    Task 2, Models #2

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,17 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
+    @NotBlank
+    @Size(min = 3, max = 100, message = "An employer can only have one location")
+    public String location;
+    //2.2
+
+    public Employer(String location) {
+        this.location = location;
+    }
 }
