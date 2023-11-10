ba3da875338510be2837dea443202e30671197e8
MrsKing2023 <smarshall84@yahoo.com>
2023-11-10 12:03:09 -0500

    SQL

--- queries.sql
+++ queries.sql
@@ -1,9 +1,14 @@
---Part 1
+Part 1
 id INT,
 employer VARCHAR(255),
 name  VARCHAR(255),
 skills  VARCHAR(255)
 --Part 2
+SELECT *,
+FROM employer,
+Where location,
+Like "St. Louis City";
+
 
 --Part 3
 
